09cf60951767924fda1b5ee2533063a77c08daa3
de-scientist <gitaumark502@gmail.com>
2025-10-02 16:13:53 +0300

    use the dot notation to access the property of an object

---
+++ object.js
@@ -0,0 +1,21 @@
+const school = {
+    nameOfSchool: "Mark II Academy",
+    location: "New York",
+    established: 2030,
+    principal: "Jane Wanjiru",
+
+}
+
+school.population = 5000;
+school.motto = "Learn, Earn, Return";
+
+console.log(school);
+
+/*school.form1.classTeacher = "Mr. Isaac";
+school.form2.classTeacher = "Ms. Hildah";
+school.form3.classTeacher = "Mr. Ndegwa";
+school.form4.classTeacher = "Mrs. Joan";*/
+
+//To access a property of an object we use the dot notation
+console.log(school.nameOfSchool);
+
