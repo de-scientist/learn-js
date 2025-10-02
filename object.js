e2254e125881bdcb6653ee5af5150eb100dd8493
de-scientist <gitaumark502@gmail.com>
2025-10-02 16:18:54 +0300

    add object.freeze() method freezes an object

--- object.js
+++ object.js
@@ -19,3 +19,10 @@ school.form4.classTeacher = "Mrs. Joan";*/
 //To access a property of an object we use the dot notation
 console.log(school.nameOfSchool);
 
+//object.freeze() method freezes an object. A frozen object can no longer be changed or modified.
+const simpleObject = {
+    nameJ: "John",
+    age: 25
+}
+Object.freeze(simpleObject);
+simpleObject.age = 30; // This will not change the age property
