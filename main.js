28b3cabc85ba1f8f5645c17bc326037020651394
de-scientist <gitaumark502@gmail.com>
2025-10-02 10:17:11 +0300

    find largest number among 3

--- main.js
+++ main.js
@@ -75,3 +75,16 @@ function canIDrive (username, age) {
 console.log(canIDrive('Mark', 23));
 console.log(canIDrive('Jane', 15));
 ;
+
+//Find largest of three numbers
+function findLargest (num1, num2, num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    }
+    else if (num2 >= num1 && num2 >= num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
+console.log(findLargest(25, 35, 15));
