c395757e575ac7db2a9460d884ffc53ddb07bf0a
de-scientist <gitaumark502@gmail.com>
2025-10-02 10:57:21 +0300

    add a fizzbuzz checker

--- main.js
+++ main.js
@@ -134,3 +134,19 @@ function greetUser (username, hour) {
     }
 }
 greetUser('Mark', 10);
+
+
+//FizzBuzz
+function fizzBuzzCheck (number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return 'FizzBuzz';
+    } else if (number % 3 === 0) {
+        return 'Fizz';
+    } else if (number % 5 === 0) {
+        return 'Buzz';
+    } else {
+        return String(number)
+    }
+}
+console.log(fizzBuzzCheck(15));
+console.log(fizzBuzzCheck(30));
